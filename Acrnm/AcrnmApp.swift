63a5012c17628cff866f08881b60073441c74720
Danylo Dyachok <danylodyachok@nerdzlab.com>
2024-06-27 21:01:05 +0300

    Initial Commit

---
+++ Acrnm/AcrnmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcrnmApp.swift
+//  Acrnm
+//
+//  Created by Danylo Dyachok on 27.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct AcrnmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
